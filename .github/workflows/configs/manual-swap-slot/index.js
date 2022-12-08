4dea5178050ac4c61866f511548d04edb3eafb7a
Thada Wangthammang <thada.wth@gmail.com>
2022-12-08 08:53:15 +0700

    fix: error get matrix job 3

--- .github/workflows/configs/manual-swap-slot/index.js
+++ .github/workflows/configs/manual-swap-slot/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const jsonPath = path.resolve(process.argv[2]);
 const json = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
-const outputName = 'deployment-matrix';
+const outputName = 'matrix';
 
 console.log(json);
 console.log(`::set-output name=${outputName}::${JSON.stringify(json)}`);
